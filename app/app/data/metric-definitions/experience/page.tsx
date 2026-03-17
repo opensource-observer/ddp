b47c86020e234b7dfc336ffadd4d85e58d501b3e
ccerv1 <carl.cervone@gmail.com>
2026-03-17 14:53:55 -0400

    feat: add Experience metric definition notebook with sidebar nav and route

---
+++ app/app/data/metric-definitions/experience/page.tsx
@@ -0,0 +1,9 @@
+import MarimoIframe from '@/components/MarimoIframe';
+
+export default function Experience() {
+  return (
+    <div className="h-full w-full">
+      <MarimoIframe notebookName="notebooks/data/metric-definitions/experience" />
+    </div>
+  );
+}
